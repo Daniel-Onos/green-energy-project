4a2b6fc6f46a9a98ca4e5ec363bdc986de5df748
Daniel Onokpise <onokpisedaniel01@gmail.com>
2022-07-06 14:52:56 +0100

    modifications

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,4 +1,4 @@
-document.querySelector("#loginform").addEventListener("submit", (e) => {
+document.querySelector("#form").addEventListener("submit", (e) => {
     e.preventDefault();
   
     const formData = new FormData(e.target);
